27b9c736381d2148fba92928569e63aed2df48f2
Daniel Hernandez Stalhuth <hsdaniele95@gmail.com>
2022-05-21 01:16:15 -0500

    Testing syntax

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -7,6 +7,7 @@ const { interface, bytecode } = require('../compile');
 
 let accounts;
 let inbox;
+const Initial_String = 'Hi there!';
 
 beforeEach( async () => {
     // Get a list of all accounts
@@ -14,12 +15,17 @@ beforeEach( async () => {
 
     // Use one of those accounts to deploy the contract
     inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({data: bytecode, arguments: ['Hi there!']})
+        .deploy({data: bytecode, arguments: [Initial_String]})
         .send({ from: accounts[0], gas: '1000000' });
 });
 
 describe('Inbox', () =>{
     it('deploys a contract', () => {
-        console.log(inbox);
+        assert.ok(inbox.options.address);
+    });
+    
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, Initial_String);
     });
 });
